1c83f901f6beedb216c47384468b6c0ef2e7de83
Neidz <adamo7255@gmail.com>
2023-10-30 17:49:13 +0100

    feature: adds rest of Match component handling different cases of loading, error and received data

--- frontend/src/components/match/Match.tsx
+++ frontend/src/components/match/Match.tsx
@@ -2,31 +2,59 @@ import { Box, Skeleton, Typography } from "@mui/material";
 import { ShipCard } from "../shipCard";
 import { MatchState } from "../../reducers/matchReducer";
 import { FC } from "react";
+import { STATE } from "../../constants/enums";
 
 interface Props {
     matchState: MatchState;
 }
 
-export const Match: FC<Props> = ({ matchState }) => {
-    console.log(matchState);
-    if (!matchState.firstPlayerSpaceship || !matchState.secondPlayerSpaceship) {
+export const Match: FC<Props> = ({
+    matchState: {
+        winner,
+        matchStarted,
+        selectedProperty,
+        queryLoading,
+        queryError,
+        firstPlayerScore,
+        firstPlayerSpaceship,
+        secondPlayerScore,
+        secondPlayerSpaceship,
+    },
+}) => {
+    if (queryLoading || !matchStarted) {
         return (
-            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "end", gap: "1rem" }}>
-                <Skeleton variant="rounded" width={150} height={500} />
-                <Skeleton variant="rounded" width={150} height={500} />
+            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "end", gap: "1rem", flexWrap: "wrap" }}>
+                <Skeleton variant="rounded" sx={{ width: "250px", minHeight: "400px" }} />
+                <Skeleton variant="rounded" sx={{ width: "250px", minHeight: "400px" }} />
             </Box>
         );
     }
 
+    if (queryError || !firstPlayerSpaceship || !secondPlayerSpaceship) {
+        return (
+            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "end", gap: "1rem", flexWrap: "wrap" }}>
+                <Skeleton variant="rounded" sx={{ width: "250px", minHeight: "400px", backgroundColor: "error.main" }} />
+                <Skeleton variant="rounded" sx={{ width: "250px", minHeight: "400px", backgroundColor: "error.main" }} />
+            </Box>
+        );
+    }
+
+    const firstPlayerState = winner === 1 ? STATE.WON : winner === 2 ? STATE.LOST : STATE.NEUTRAL;
+    const secondPlayerState = winner === 2 ? STATE.WON : winner === 1 ? STATE.LOST : STATE.NEUTRAL;
+
     return (
-        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "end", gap: "1rem" }}>
+        <Box sx={{ display: "flex", justifyContent: "center", alignItems: "end", gap: "1rem", flexWrap: "wrap" }}>
             <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem", textAlign: "center" }}>
-                <Typography>My score: {matchState.firstPlayerScore}</Typography>
-                <ShipCard spaceship={matchState.firstPlayerSpaceship} />
+                <Typography>My score: {firstPlayerScore}</Typography>
+                <ShipCard spaceship={firstPlayerSpaceship} highlightedProperty={selectedProperty} state={firstPlayerState} />
             </Box>
             <Box sx={{ display: "flex", flexDirection: "column", gap: "1rem", textAlign: "center" }}>
-                <Typography>Opponent: {matchState.secondPlayerScore}</Typography>
-                <ShipCard spaceship={matchState.secondPlayerSpaceship} />
+                <Typography>Opponent: {secondPlayerScore}</Typography>
+                <ShipCard
+                    spaceship={secondPlayerSpaceship}
+                    highlightedProperty={selectedProperty}
+                    state={secondPlayerState}
+                />
             </Box>
         </Box>
     );
