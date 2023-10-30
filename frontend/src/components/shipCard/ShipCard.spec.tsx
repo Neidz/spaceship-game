0d7078e9d0c18f1af19d32ead22916841b6da0ad
Neidz <adamo7255@gmail.com>
2023-10-30 01:02:38 +0100

    chore: updates ShipCard test

--- frontend/src/components/shipCard/ShipCard.spec.tsx
+++ frontend/src/components/shipCard/ShipCard.spec.tsx
@@ -17,13 +17,10 @@ describe("ShipCard Component", () => {
     it("renders the ShipCard component with provided spaceship data", () => {
         const { container } = render(<ShipCard spaceship={sampleSpaceship} />);
 
-        expect(container.textContent).toContain("Nebula Nomad");
-        expect(container.textContent).toContain("Crew size: 46");
-        expect(container.textContent).toContain("Max speed: 10809");
-        expect(container.textContent).toContain("Range: 199600");
-        expect(container.textContent).toContain("Cargo capacity: 737");
-        expect(container.textContent).toContain("Cost: 55405794");
-        expect(container.textContent).toContain("Weight: 24125043");
-        expect(container.textContent).toContain("Battery capacity: 1256868");
+        Object.keys(sampleSpaceship).forEach((key) => {
+            if (key !== "id") {
+                expect(container.textContent).toContain(sampleSpaceship[key as keyof typeof sampleSpaceship].toString());
+            }
+        });
     });
 });
